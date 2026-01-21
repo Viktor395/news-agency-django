b0a6fe116bfb551eed2d2541334fd384596a2c11
Viktor Velychkocd <velviktor831@gmail.com>
2026-01-21 22:48:12 +0200

    Implement home page view and news list template

--- news/views.py
+++ news/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404 # Додали get_object_or_404
+from .models import Article
 
-# Create your views here.
+def index(request):
+    # Отримуємо всі статті, від нових до старих
+    articles = Article.objects.all().order_by('-published_date')
+    return render(request, 'news/index.html', {'articles': articles})
+
+def article_detail(request, slug):
+    # Ця функція шукає статтю за її "slug". 
+    # Якщо не знайде — видасть помилку 404 (сторінка не знайдена).
+    article = get_object_or_404(Article, slug=slug)
+    return render(request, 'news/article_detail.html', {'article': article})
