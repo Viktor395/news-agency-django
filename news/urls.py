6f04bd20a6e3c86ee7dbfe09193e7b5b1f3fac8b
Viktor Velychkocd <velviktor831@gmail.com>
2026-01-21 23:18:11 +0200

    Enhance article detail template with Bootstrap and breadcrumbs

--- news/urls.py
+++ news/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    # Важливо, щоб назва була в лапках і точно збігалася
+    path('article/<slug:slug>/', views.article_detail, name='article_detail'),
 ]
