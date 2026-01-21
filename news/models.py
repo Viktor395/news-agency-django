428e032ce39d00499bc6c89318146e81b9b2d7bd
Viktor Velychkocd <velviktor831@gmail.com>
2026-01-21 15:22:47 +0200

    Add news models, initial migrations and setup gitignore

--- news/models.py
+++ news/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=100, unique=True)
+
+    class Meta:
+        verbose_name_plural = "Categories"
+
+    def __str__(self):
+        return self.name
+
+class Article(models.Model):
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255, unique=True)
+    content = models.TextField()
+    image = models.ImageField(upload_to='articles_images/')
+    published_date = models.DateTimeField(auto_now_add=True)
+    
+    # Зв'язки (Relationships)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
