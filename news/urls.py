e00ca44da1bd7bc7d2d230e3a17f603e52bde641
Viktor Velychkocd <velviktor831@gmail.com>
2026-01-22 15:32:13 +0200

    Implement user registration functionality with form and view

--- news/urls.py
+++ news/urls.py
@@ -3,6 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    # Важливо, щоб назва була в лапках і точно збігалася
     path('article/<slug:slug>/', views.article_detail, name='article_detail'),
 ]
--- news/views.py
+++ news/views.py
@@ -1,13 +1,12 @@
-from django.shortcuts import render, get_object_or_404 # Додали get_object_or_404
+from django.shortcuts import render, get_object_or_404
 from .models import Article
 
 def index(request):
-    # Отримуємо всі статті, від нових до старих
+    
     articles = Article.objects.all().order_by('-published_date')
     return render(request, 'news/index.html', {'articles': articles})
 
 def article_detail(request, slug):
-    # Ця функція шукає статтю за її "slug". 
-    # Якщо не знайде — видасть помилку 404 (сторінка не знайдена).
+    
     article = get_object_or_404(Article, slug=slug)
     return render(request, 'news/article_detail.html', {'article': article})
