28aa8db8ee4067026f34e0436523a7e0cfa6004d
Viktor Velychkocd <velviktor831@gmail.com>
2026-02-07 13:45:27 +0200

    Remove superuser creation from build script

--- build.sh
+++ build.sh
@@ -4,4 +4,3 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
-python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(username='Viktor').exists() or User.objects.create_superuser('Viktor', 'vel_v@ukr.net', 'MaxPol777')"
