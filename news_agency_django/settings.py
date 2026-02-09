b3da617119e416680bcd19eda4066f4c17579fed
Viktor Velychkocd <velviktor831@gmail.com>
2026-02-09 19:04:38 +0200

    Force static serving in urls.py

--- news_agency_django/settings.py
+++ news_agency_django/settings.py
@@ -135,7 +135,7 @@ USE_TZ = True
 STATIC_URL = "/static/"
 STATICFILES_DIRS = [BASE_DIR / "static"]
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
+STATICFILES_STORAGE = "whitenoise.storage.StaticFilesStorage"
 
 
 MEDIA_URL = "/media/"
