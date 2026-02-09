2e2af3ecacfa1bc368f2f08adfe278861846e240
Viktor Velychkocd <velviktor831@gmail.com>
2026-02-09 18:43:41 +0200

    Change whitenoise storage to fix admin styles

--- news_agency_django/settings.py
+++ news_agency_django/settings.py
@@ -149,7 +149,6 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 LOGIN_REDIRECT_URL = "/"
 LOGOUT_REDIRECT_URL = "/"
 
-import os
 
 CLOUDINARY_STORAGE = {
     'CLOUD_NAME': os.environ.get('CLOUDINARY_CLOUD_NAME'),
