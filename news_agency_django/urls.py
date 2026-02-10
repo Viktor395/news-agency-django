2e9e034e5161815b126136fa524f7016a3c0bd69
Viktor Velychkocd <velviktor831@gmail.com>
2026-02-10 17:11:15 +0200

    Fix static paths and folder colors

--- news_agency_django/urls.py
+++ news_agency_django/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.views.static import serve # Додайте цей імпорт
+from django.views.static import serve 
 import re
 
 urlpatterns = [
@@ -11,12 +11,12 @@ urlpatterns = [
     path('', include('news.urls')),
 ]
 
-# Це дозволить Django віддавати медіа-файли (фото з Cloudinary) та статику
+
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 else:
-    # "План Б": якщо WhiteNoise не справляється, Django сам знайде статику в STATIC_ROOT
+
     urlpatterns += [
         path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
     ]
