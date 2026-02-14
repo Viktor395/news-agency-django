299b36dc1d1b771d2e02e501992b588608ca2bdd
Viktor Velychkocd <velviktor831@gmail.com>
2026-02-14 10:42:50 +0200

    Fix static and media serving for production on Render

--- news_agency_django/urls.py
+++ news_agency_django/urls.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.static import serve 
-import re
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -11,12 +10,11 @@ urlpatterns = [
     path('', include('news.urls')),
 ]
 
-
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 else:
-
     urlpatterns += [
-        path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
+        re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
+        re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
     ]
